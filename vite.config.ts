15b21b789f30ced396f53881e01eb8be3bc786ff
Joseph9866 <151791315+Joseph9866@users.noreply.github.com>
2025-07-16 14:30:06 +0300

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 });
